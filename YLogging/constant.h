e0a81841cca057c50e38f8ece3431a538bc0646a
salmangt <salman.ahmed@gaditek.com>
2018-04-16 14:11:05 +0500

    add testing files

---
+++ YLogging/constant.h
@@ -0,0 +1,13 @@
+//
+//  constant.h
+//  YLogging
+//
+//  Created by Salman  Ahmed on 16/04/2018.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface constant : NSObject
+
+@end
---
+++ YLogging/constant.m
@@ -0,0 +1,13 @@
+//
+//  constant.m
+//  YLogging
+//
+//  Created by Salman  Ahmed on 16/04/2018.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+#import "constant.h"
+
+@implementation constant
+
+@end
